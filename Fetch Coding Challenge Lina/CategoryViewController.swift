5f7efaad4e084882f456f98a1479c82eaca3003b
Lina Wassong <lina.wassong@gmail.com>
2022-05-24 11:35:23 -0700

    initial project

---
+++ Fetch Coding Challenge Lina/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Fetch Coding Challenge Lina
+//
+//  Created by Lina on 5/24/22.
+//
+
+import Foundation
+import UIKit
+
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+    }
+
+
+}
---
+++ Fetch Coding Challenge Lina/SceneDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  SceneDelegate.swift
+//  Fetch Coding Challenge Lina
+//
+//  Created by Lina on 5/24/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        //programmatically setup app with Scene Delegate and create window hierarchy 
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = CategoryViewController()
+        let navigation = UINavigationController(rootViewController: viewController)
+
+        window.rootViewController = navigation
+        self.window = window
+        window.makeKeyAndVisible()
+
+
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
