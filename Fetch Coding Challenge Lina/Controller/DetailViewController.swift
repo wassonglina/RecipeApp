731442adedf4043a4d76109fa20197506ee64e91
Lina Wassong <lina.wassong@gmail.com>
2022-05-27 10:13:08 -0700

    prepare all labels in detail vc stakc view

--- Fetch Coding Challenge Lina/Controller/DetailViewController.swift
+++ Fetch Coding Challenge Lina/Controller/DetailViewController.swift
@@ -23,13 +23,10 @@ class DetailViewController: UIViewController {
         addStackView()
         addTitleLabel()
         addImageView()
-
-        let label2 = UILabel()
-        label2.text = "Ingredients"
-        label2.font = .boldSystemFont(ofSize: 19)
-        label2.backgroundColor = .yellow
-        label2.numberOfLines = 0
-        stackView.addArrangedSubview(label2)
+        addIngredientsTitleLabel()
+        addIngredientStackView()
+        addInstructionTitleLabel()
+        addInstructionsLabel()
     }
 
     private func addStackView() {
@@ -37,7 +34,7 @@ class DetailViewController: UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.spacing = 20
+        stackView.spacing = 30
 
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
@@ -61,12 +58,12 @@ class DetailViewController: UIViewController {
     }
 
     private func addTitleLabel() {
-        let label = UILabel()
-        label.text = "Chocolate Cake"
-        label.font = .boldSystemFont(ofSize: 24)
-        label.backgroundColor = .magenta
-        label.numberOfLines = 0
-        stackView.addArrangedSubview(label)
+        let titleLabel = UILabel()
+        titleLabel.text = "Chocolate Cake"
+        titleLabel.font = .boldSystemFont(ofSize: 24)
+        titleLabel.backgroundColor = .magenta
+        titleLabel.numberOfLines = 0
+        stackView.addArrangedSubview(titleLabel)
     }
 
     private func addImageView() {
@@ -75,4 +72,45 @@ class DetailViewController: UIViewController {
         stackView.addArrangedSubview(imageView)
     }
 
+    private func addIngredientsTitleLabel() {
+        let ingredientsLabel = UILabel()
+        ingredientsLabel.text = "Ingredients"
+        ingredientsLabel.font = .boldSystemFont(ofSize: 19)
+        ingredientsLabel.backgroundColor = .yellow
+        stackView.addArrangedSubview(ingredientsLabel)
+    }
+
+    private func addIngredientStackView() {
+
+        let ingredientStackView = UIStackView()
+        stackView.addArrangedSubview(ingredientStackView)
+        ingredientStackView.spacing = 25
+        ingredientStackView.backgroundColor = .systemMint
+
+        let measurementLabel = UILabel()
+        ingredientStackView.addArrangedSubview(measurementLabel)
+        measurementLabel.text = "Measurement"
+
+        let ingredientLabel = UILabel()
+        ingredientStackView.addArrangedSubview(ingredientLabel)
+        ingredientLabel.text = "Ingredients"
+    }
+
+    private func addInstructionTitleLabel() {
+        let instructionLabel = UILabel()
+        instructionLabel.text = "Instructions"
+        instructionLabel.font = .boldSystemFont(ofSize: 19)
+        instructionLabel.backgroundColor = .yellow
+        stackView.addArrangedSubview(instructionLabel)
+    }
+
+    private func addInstructionsLabel() {
+        let instructionLabel = UILabel()
+        instructionLabel.numberOfLines = 0
+        stackView.addArrangedSubview(instructionLabel)
+        instructionLabel.backgroundColor = .link
+        instructionLabel.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -70).isActive = true  //TODO: check for landscape
+        instructionLabel.text = "Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling. Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling. Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling. Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling."
+    }
 }
+
